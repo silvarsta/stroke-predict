c42fc207485b86c187084e2fd9e5caf6a430ea68
Tatya Hanum Pramudita <113042083+velottya@users.noreply.github.com>
2024-05-16 22:39:48 +0700

    update final

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -47,17 +47,17 @@
                     <div class="single_feature">
                         <div class="single_feature_part">
                             <span class="single_feature_icon"><img src="template/img/icon/feature_1.svg" alt=""></span>
-                            <h4>Better Future</h4>
-                            <p>Darkness multiply rule Which from without life creature blessed
-                                give moveth moveth seas make day which divided our have.</p>
+                            <h4>Jenis-Jenis Stroke</h4>
+                            <p>- Stroke iskemik: Terjadi ketika pembuluh darah yang memasok darah ke otak tersumbat oleh gumpalan darah atau plak.
+                                <br>- Stroke hemoragik: Terjadi ketika pembuluh darah di otak pecah, menyebabkan perdarahan di dalam atau sekitar otak.
+                                </p>
                         </div>
                     </div>
                     <div class="single_feature">
                         <div class="single_feature_part">
                             <span class="single_feature_icon"><img src="template/img/icon/feature_2.svg" alt=""></span>
-                            <h4>Better Future</h4>
-                            <p>Darkness multiply rule Which from without life creature blessed
-                                give moveth moveth seas make day which divided our have.</p>
+                            <h4>Gejala Stroke</h4>
+                            <p>Gejala umum stroke termasuk kelemahan mendadak atau mati rasa di wajah, lengan, atau kaki (terutama pada satu sisi tubuh), kebingungan, kesulitan berbicara atau memahami pembicaraan, gangguan penglihatan pada satu atau kedua mata, dan kehilangan keseimbangan atau koordinasi.</p>
                         </div>
                     </div>
                 </div>
@@ -70,17 +70,15 @@
                     <div class="single_feature">
                         <div class="single_feature_part">
                             <span class="single_feature_icon"><img src="template/img/icon/feature_1.svg" alt=""></span>
-                            <h4>Better Future</h4>
-                            <p>Darkness multiply rule Which from without life creature blessed
-                                give moveth moveth seas make day which divided our have.</p>
+                            <h4>Faktor Risiko Stroke</h4>
+                            <p>Faktor risiko utama meliputi tekanan darah tinggi (hipertensi), penyakit jantung, diabetes, merokok, obesitas, dan gaya hidup sedentari. Faktor genetik dan usia lanjut juga meningkatkan risiko terkena stroke.</p>
                         </div>
                     </div>
                     <div class="single_feature">
                         <div class="single_feature_part">
                             <span class="single_feature_icon"><img src="template/img/icon/feature_2.svg" alt=""></span>
-                            <h4>Better Future</h4>
-                            <p>Darkness multiply rule Which from without life creature blessed
-                                give moveth moveth seas make day which divided our have.</p>
+                            <h4>Pencegahan Stroke</h4>
+                            <p>Faktor risiko utama meliputi tekanan darah tinggi (hipertensi), penyakit jantung, diabetes, merokok, obesitas, dan gaya hidup sedentari. Faktor genetik dan usia lanjut juga meningkatkan risiko terkena stroke.</p>
                         </div>
                     </div>
                 </div>
